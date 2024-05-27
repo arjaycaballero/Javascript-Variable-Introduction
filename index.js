dd9e8f39c5453aadb3a18d6e990c70c45da92b93
Arjay Caballero <130941463+arjaycaballero@users.noreply.github.com>
2024-05-27 20:47:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// prompt allows the user to input something, namely a piece of text.
+var myName = "Arjay";
+// var is a keyword just like alert or prompt. this tells that we will create a new variable which is myName
+// var means variable.
+var yourName = "Michelle";
+alert("my name is "+ myName +" and my girlfriend name is "+ yourName);
